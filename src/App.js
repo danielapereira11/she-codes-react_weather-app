93c5db02af13f5e81b575493c4f399fd83d2439b
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2022-04-01 22:40:55 +0100

    Removed Forecast section for now

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./App.css";
 import Footer from "./Footer.js";
 import MainRow from "./MainRow.js";
-import Forecast from "./Forecast";
 
 function App() {
   return (
     <div className="App">
       <MainRow city="Guimarães" />
-      {/* <Forecast /> */}
       <br />
       <Footer />
     </div>
